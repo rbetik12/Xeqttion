1fcbfd538347a199fe9c3a14669ce8d6ba5ee9ba
Gleb Korzan <gleb.korzan@gmail.com>
2019-08-03 12:27:49 -0700

    created basic project file structure

---
+++ android/src/com/proces/xeqttion/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.proces.xeqttion;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.proces.xeqttion.Xeqttion;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Xeqttion(), config);
+	}
+}
