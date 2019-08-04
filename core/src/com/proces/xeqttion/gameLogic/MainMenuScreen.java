a8ce346a62f38d26972f1825552ac19b3c3152ab
Gleb Korzan <gleb.korzan@gmail.com>
2019-08-04 14:04:24 -0700

    created launch screen

--- core/src/com/proces/xeqttion/gameLogic/MainMenuScreen.java
+++ core/src/com/proces/xeqttion/gameLogic/MainMenuScreen.java
@@ -1,6 +1,63 @@
 package com.proces.xeqttion.gameLogic;
 
-public class MainMenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenuScreen implements Screen {
 
     final GameClass game;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final GameClass game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480); // nailed size of screen
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcomen!", 100, 150);
+        game.font.draw(game.batch, "Tap to comtinue!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
