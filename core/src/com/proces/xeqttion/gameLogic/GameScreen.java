b3befab16e64be9ddac0df9aa4c918c3174bc250
Vitaliy <mr.prikota@gmail.com>
2019-08-05 01:22:32 +0300

    Added clicks handler for game screen

--- core/src/com/proces/xeqttion/gameLogic/GameScreen.java
+++ core/src/com/proces/xeqttion/gameLogic/GameScreen.java
@@ -7,21 +7,25 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameScreen implements Screen {
-    private int pointsAmount;
+    final private GameClass game;
     private Stage stage;
 
     public GameScreen(GameClass game) {
-        pointsAmount = game.getPointsAmount();
+        this.game = game;
     }
 
     @Override
     public void show() {
         stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("trueTypeFonts/Roboto-Regular.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 50;
@@ -32,9 +36,18 @@ public class GameScreen implements Screen {
         Label.LabelStyle labelStyle = new Label.LabelStyle();
         labelStyle.font = font;
 
-        Label pointsLabel = new Label(String.valueOf(pointsAmount), labelStyle);
-        pointsLabel.setPosition(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() * 0.9f);
+        final Label pointsLabel = new Label(String.valueOf(game.getPointsAmount()), labelStyle);
+        pointsLabel.setPosition(Gdx.graphics.getWidth() / 2f - pointsLabel.getScaleX() / 2f, Gdx.graphics.getHeight() * 0.9f);
         stage.addActor(pointsLabel);
+
+        stage.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.incPointsAmount();
+                pointsLabel.setText(game.getPointsAmount());
+                return true;
+            }
+        });
     }
 
     @Override
