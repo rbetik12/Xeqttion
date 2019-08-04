599394c4d6cc2f459816b974cb323c251d3a4273
Vitaliy <mr.prikota@gmail.com>
2019-08-04 22:44:07 +0300

    Added new dependencies and start screen touch handler

--- android/src/com/proces/xeqttion/AndroidLauncher.java
+++ android/src/com/proces/xeqttion/AndroidLauncher.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.proces.xeqttion.Xeqttion;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Xeqttion(), config);
-	}
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new Xeqttion(), config);
+    }
 }
